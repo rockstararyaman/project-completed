7f8930e6132a1d0dcccf4a0a982be1982916b2a7
rockstararyaman <93260229+rockstararyaman@users.noreply.github.com>
2023-04-22 10:53:03 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,49 @@
+img = "";
+Status = "";
+objects = [];
+
+function preload() {
+    img = loadImage('bed.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(670, 470);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+
+    image(img, 0, 0, 670, 470);
+
+    if (Status != "") {
+
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+
+    }
+
+}
+
+function modelLoaded() {
+
+    console.log('Model is Loaded!!');
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
